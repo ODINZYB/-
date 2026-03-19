deaa2dfb9ce0cca20604d059b6f9be39a4bb7a53
ODINZYB <ODINZYB@users.noreply.github.com>
2026-03-19 23:42:02 +0800

    simplify run script

--- run.bat
+++ run.bat
@@ -1,22 +1,10 @@
 @echo off
 
-echo Starting Backend Setup...
 cd backend
 call npm install
-echo Starting Backend Server...
-start "Backend" cmd /k "node index.js"
-cd ..
+start node index.js
 
-echo.
-echo Starting Frontend Setup...
-cd frontend
+cd ../frontend
 call npm install
-echo Building Frontend...
 call npm run build
-echo Starting Frontend Server...
-start "Frontend" cmd /k "npm run start"
-cd ..
-
-echo.
-echo Setup Complete!
-pause
+start npm run start
